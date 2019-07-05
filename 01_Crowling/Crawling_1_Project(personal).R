e51e9f012405c52f970aef3e926b47ac5b8a6828
blockhead4 <blockhead4@naver.com>
2019-07-05 17:33:28 +0900

    Crawling_Project

--- 01_Crowling/Crawling_1_Project(personal).R
+++ 01_Crowling/Crawling_1_Project(personal).R
@@ -1,21 +1,31 @@
-# NAVER 영화 ('스파이더맨 : 파프롬 홈') 일반인 리뷰 크롤링
+# NAVER 영화 [ 스파이더맨 : 파프롬 홈 ] 일반인 리뷰 크롤링
+
+# 1. 분석에 필요한 환경 구성
+Sys.setenv(JAVA_HOME="C:/Program Files/Java/jdk1.8.0_202/")
+#install.packages("rJava")
+library(rJava)
+library(KoNLP)
 library(rvest)
 library(stringr)
 library(dplyr)
 library(xlsx)
-library(rJava)
-library(KoNLP)
 library(wordcloud)
 library(RColorBrewer)
 library(ggplot2)
+library(extrafont)
+windowsFonts(myfont = "맑은 고딕")
+theme_update(text=element_text(family="myfont"))
 useSejongDic()
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+setwd("D:/workspace-Jwp/R/R-Project/01_Crowling")
 
-ifr_base_url <- 'https://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn?code=173123&type=after&isActualPointWriteExecute=false&isMileageSubscriptionAlready=false&isMileageSubscriptionReject=false&page='
-pages <- c(1:336)
+# 2. 평점 페이지 크롤링
+ifr_base_url <- "https://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn?code=173123&type=after&onlyActualPointYn=N&order=newest&page="
+pages <- c(30:421)
 
-points <- c()
-for(i in 1:length(pages)) {
+# 평점, 리뷰내용, 작성자, 시간 데이터를 크롤링하여 엑셀로 저장
+points1 <- c()
+for(i in 30:max(pages)) {
   url <- paste0(ifr_base_url, pages[i])
   html <- read_html(url)
   
@@ -41,37 +51,38 @@ for(i in 1:length(pages)) {
     tmp <- trim(str_sub(tmp, idx[1], -1))
     idx <- str_locate(tmp, "\r")
     time <- c(time, str_sub(tmp, 1, idx[1]-1))
-    #print(time)
   }
   
   result <- data.frame(score=score, review=review, writer=writer, time=time)
-  points <- rbind.data.frame(points, result)
+  points1 <- rbind.data.frame(points1, result)
 }
-points
+head(points1)
 
-write.xlsx(points, "D:/workspace-Jwp/R/R-Project/01_Crowling/data/points.xlsx")
+write.xlsx(points1, "data/points1.xlsx")
+points1 <- read.xlsx("data/points1.xlsx", 1, encoding="UTF-8")
 
-# WordCloud 
-setwd("D:/workspace-Jwp/R/R-Project/01_Crowling")
-head(points$review)
-reply <- points$review
-write.csv(reply, "data/reply.csv", row.names = F)
-words <- readLines("data/reply.csv")
+# 3. WordCloud
+# 리뷰 내용 전처리
+head(points1$review)
+reply <- points1$review
+write.csv(reply, "data/reply1.csv", row.names = F)
+lines_r <- readLines("data/reply1.csv")
 
-words <- sapply(words, extractNoun, USE.NAMES = F)
+words <- sapply(lines_r, extractNoun, USE.NAMES = F)
+
 cdata <- unlist(words)
 words <- str_replace_all(cdata, "[^[:alpha:]]", "")
 words <- gsub(" ", "", words)
 words <- gsub("\\d+", "", words)
 words <- Filter(function(x) {nchar(x) >= 2}, words)
-head(words, 20)  
+words <- Filter(function(x) {nchar(x) <= 5}, words)
+head(words, 20)
 
-write(unlist(words), "data/reply.txt")
-rev <- read.table("data/reply.txt")
-rev
+write(unlist(words), "data/reply1.txt")
+rev <- read.table("data/reply1.txt")
 
 wordcount <- table(rev)
-head(sort(wordcount, decreasing = T), 100)
+head(sort(wordcount, decreasing = T), 30)
 
 words <- gsub("영화", "", words)
 words <- gsub("진짜", "", words)
@@ -81,35 +92,35 @@ words <- gsub("이번", "", words)
 words <- gsub("뭔가", "", words)
 words <- gsub("이후", "", words)
 words <- gsub("스파이더맨", "", words)
+words <- gsub("관람객", "", words)
 
-write(unlist(words), "data/reply.txt")
-rev <- read.table("data/reply.txt")
+write(unlist(words), "data/reply1.txt")
+rev <- read.table("data/reply1.txt")
 
 wordcount <- table(rev)
-head(sort(wordcount, decreasing = T), 20)
+head(sort(wordcount, decreasing = T), 30)
 
-windows()
+# Word Cloud Output
+#windows()
 windowsFonts(font=windowsFont("맑은 고딕"))
 
 palete <- brewer.pal(10, 'Paired')
 wordcloud(names(wordcount), freq=wordcount, scale = c(4,0.4), rot.per = 0.1,
-          min.freq = 3, random.order = F, random.color = T, colors = palete, family="font")
-#legend(0.1, 0.92, "NAVER 영화 [스파이더맨 : 파 프롬 홈]\n평점 댓글 WordCloud(19.07.02 ~ 19.07.04)", cex=1, fill=NA, border=NA,
-#       bg='navy', text.col='green', text.font=2, box.col='white')
+          min.freq = 4, random.order = F, random.color = T, colors = palete, family="font")
 
-
-# Star Rating Change over time
-# Change by Date
-head(points)
-star_date <- select(points, score, time)
-
+# 4. Time-Series Graph (Star Rating Change over time)
+# 1) Change by Date
+# 데이터 전처리
+star_date <- select(points1, score, time)
 tmp <- strptime(star_date$time, "%Y.%m.%d %H")
 tmp <- paste0(tmp$mon+1, "월 ", tmp$mday, "일")
 star_date$time <- tmp
-star_date <- filter(star_date, str_sub(star_date$time, 1, 5) != "7월 5일")
+star_date <- filter(star_date, "7월 2일" == str_sub(star_date$time, 1, 5) |
+                               "7월 3일" == str_sub(star_date$time, 1, 5) |
+                               "7월 4일" == str_sub(star_date$time, 1, 5))
 star_date$score <- factor(star_date$score, levels=c(10:1))
-levels(star_date$score)
 
+# 날짜(일) 및 평점별 빈도수
 star_date_bar <- star_date %>%
   group_by(time, score) %>%
   summarise(score_freq = n()) %>%
@@ -124,18 +135,14 @@ star_date_bar2 <- star_date_bar %>%
   arrange(time, desc(score))
 star_date_bar2
 
-library(extrafont)
-windowsFonts(myfont = "맑은 고딕")
-theme_update(text=element_text(family="myfont"))
-
 # Bar Chart
 ggplot(data=star_date_bar2, aes(x=time, y=score_freq, fill=score)) + 
   geom_bar(stat="identity") +
   geom_text(aes(y=ypos, label=ylabel_b), cex=4, color="black", family="myfont") +
   theme_light() +
-  xlab("날짜(일)") +
+  xlab("날짜(단위: 일)") +
   ylab("평점(1~10점)") +
-  ggtitle("[스파이더맨 : 파 프롬 홈]\n평점 빈도(19.07.02 ~ 19.07.04)") +
+  ggtitle("[ 스파이더맨 : 파 프롬 홈 ]\n평점 빈도 (19.07.02 ~ 19.07.04)") +
   theme(plot.title=element_text(size=rel(1.5), face="bold", hjust=0.5, family="myfont")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -146,15 +153,16 @@ ggplot(data=star_date_bar2, aes(x=time, y=score_freq, fill=score)) +
   geom_bar(stat="identity", position="dodge") +
   theme_light() +
   geom_text(aes(y=rep(seq(360, 900, 60), 3), label=ylabel_b), cex=4, color="navy", family="myfont") +
-  xlab("날짜(일)") +
+  xlab("날짜(단위: 일)") +
   ylab("평점(1~10점)") +
-  ggtitle("[스파이더맨 : 파 프롬 홈]\n평점 빈도(19.07.02 ~ 19.07.04)") +
+  ggtitle("[ 스파이더맨 : 파 프롬 홈 ]\n평점 빈도 (19.07.02 ~ 19.07.04)") +
   theme(plot.title=element_text(size=rel(1.5), face="bold", hjust=0.5, family="myfont")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         axis.title.x = element_text(size=rel(1.2), family="myfont"),
         axis.title.y = element_text(size=rel(1.2), family="myfont"))
 
+# 날짜(일)별 평균 평점
 star_date$score <- as.character(star_date$score)
 star_date$score <- as.numeric(star_date$score)
 
@@ -167,36 +175,40 @@ star_date_line
 ggplot(star_date_line, aes(x=time, y=avg_score, color="red", group=1)) + 
   geom_line(color="red", size=1) +
   theme_light() +
-  xlab("날짜(일)") +
+  xlab("날짜(단위: 일)") +
   ylab("평점(1~10점)") +
-  ggtitle("[스파이더맨 : 파 프롬 홈]\n날짜에 따른 평점(평균) 변화(19.07.02 ~ 19.07.04)") +
+  ggtitle("[ 스파이더맨 : 파 프롬 홈 ]\n날짜에 따른 평점(평균) 변화 (19.07.02 ~ 19.07.04)") +
   theme(plot.title=element_text(size=rel(1.5), face="bold", hjust=0.5, family="myfont")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         axis.title.x = element_text(size=rel(1.2), family="myfont"),
         axis.title.y = element_text(size=rel(1.2), family="myfont"))
 
-# Change Hourly
-# Line Chart(1시간 단위)
-star_date2 <- select(points, score, time)
+# 2) Change Hourly
+# 데이터 전처리
+star_date2 <- select(points1, score, time)
 tmp <- strptime(star_date2$time, "%Y.%m.%d %H:%M")
 tmp <- paste0(tmp$mon+1, "월 ", tmp$mday, "일 ", tmp$hour, "시")
 star_date2$time <- tmp
-star_date2 <- filter(star_date2, str_sub(star_date2$time, 1, 5) != "7월 5일")
+star_date2 <- filter(star_date2, "7월 2일" == str_sub(star_date2$time, 1, 5) |
+                                 "7월 3일" == str_sub(star_date2$time, 1, 5) |
+                                 "7월 4일" == str_sub(star_date2$time, 1, 5))
 star_date2$score <- as.character(star_date2$score)
 star_date2$score <- as.numeric(star_date2$score)
 
+# 시간(1시간)별  평균 평점
 star_hour_line1 <- star_date2 %>%
   group_by(time) %>%
   summarise(avg_score = mean(score))
 star_hour_line1
 
+# Line Chart(1시간 단위)
 ggplot(star_hour_line1, aes(x=time, y=avg_score, group=1)) + 
   geom_line(color="red", lwd=1) +
   theme_light() +
   xlab("시간(단위: 1시간)") +
   ylab("평점(1~10점)") +
-  ggtitle("[스파이더맨 : 파 프롬 홈]\n시간에 따른 평점(평균) 변화(19.07.02 ~ 19.07.04)") +
+  ggtitle("[ 스파이더맨 : 파 프롬 홈 ]\n시간에 따른 평점(평균) 변화 (19.07.02 ~ 19.07.04)") +
   theme(plot.title=element_text(size=rel(1.5), face="bold", hjust=0.5, family="myfont")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -205,7 +217,7 @@ ggplot(star_hour_line1, aes(x=time, y=avg_score, group=1)) +
         axis.text.x = element_blank())
 
 # Line Chart(4시간 단위)
-star_date2 <- select(points, score, time)
+star_date2 <- select(points1, score, time)
 tmp <- strptime(star_date2$time, "%Y.%m.%d %H:%M")
 
 time_tmp <- rep(NA, length(tmp))
@@ -217,10 +229,11 @@ for(i in 1:length(tmp)) {
   ifelse(tmp$hour[i] <= 20, time_tmp[i] <- paste0(tmp[i]$mon+1, "월 ", tmp[i]$mday, "일 ", "16~20시"),
   time_tmp[i] <- paste0(tmp$mon[i]+1, "월 ", tmp$mday[i], "일 ", "21~24시"))))))
 }
-time_tmp
 
 star_date2$time <- time_tmp
-star_date2 <- filter(star_date2, str_sub(star_date2$time, 1, 5) != "7월 5일")
+star_date2 <- filter(star_date2, "7월 2일" == str_sub(star_date2$time, 1, 5) |
+                                 "7월 3일" == str_sub(star_date2$time, 1, 5) |
+                                 "7월 4일" == str_sub(star_date2$time, 1, 5))
 star_date2$score <- as.character(star_date2$score)
 star_date2$score <- as.numeric(star_date2$score)
 
@@ -234,7 +247,7 @@ ggplot(star_hour_line2, aes(x=time, y=avg_score, group=1)) +
   theme_light() +
   xlab("시간(단위: 4시간)") +
   ylab("평점(1~10점)") +
-  ggtitle("[스파이더맨 : 파 프롬 홈]\n시간에 따른 평점(평균) 변화(19.07.02 ~ 19.07.04)") +
+  ggtitle("[ 스파이더맨 : 파 프롬 홈 ]\n시간에 따른 평점(평균) 변화 (19.07.02 ~ 19.07.04)") +
   theme(plot.title=element_text(size=rel(1.5), face="bold", hjust=0.5, family="myfont")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
--- 01_Crowling/Crawling_3_Project(personal).R
+++ 01_Crowling/Crawling_3_Project(personal).R
@@ -1,21 +1,27 @@
-# NAVER 영화 ('스파이더맨 : 파프롬 홈') 일반인 리뷰 크롤링
+# NAVER 영화 [ 알라딘 ] 일반인 리뷰 크롤링
+Sys.setenv(JAVA_HOME="C:/Program Files/Java/jdk1.8.0_202/")
+install.packages("rJava")
+library(rJava)
+library(KoNLP)
 library(rvest)
 library(stringr)
 library(dplyr)
 library(xlsx)
-library(rJava)
-library(KoNLP)
 library(wordcloud)
 library(RColorBrewer)
 library(ggplot2)
+library(extrafont)
+windowsFonts(myfont = "맑은 고딕")
+theme_update(text=element_text(family="myfont"))
 useSejongDic()
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+setwd("D:/workspace-Jwp/R/R-Project/01_Crowling")
 
-ifr_base_url <- 'https://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn?code=173123&type=after&isActualPointWriteExecute=false&isMileageSubscriptionAlready=false&isMileageSubscriptionReject=false&page='
-pages <- c(1:336)
+ifr_base_url <- "https://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn?code=163788&type=after&onlyActualPointYn=N&order=newest&page="
+pages <- c(1:76)
 
-points <- c()
-for(i in 1:length(pages)) {
+points3 <- c()
+for(i in 1:max(pages)) {
   url <- paste0(ifr_base_url, pages[i])
   html <- read_html(url)
   
@@ -41,37 +47,36 @@ for(i in 1:length(pages)) {
     tmp <- trim(str_sub(tmp, idx[1], -1))
     idx <- str_locate(tmp, "\r")
     time <- c(time, str_sub(tmp, 1, idx[1]-1))
-    #print(time)
   }
   
   result <- data.frame(score=score, review=review, writer=writer, time=time)
-  points <- rbind.data.frame(points, result)
+  points3 <- rbind.data.frame(points3, result)
 }
-points
+head(points3)
 
-write.xlsx(points, "D:/workspace-Jwp/R/R-Project/01_Crowling/data/points.xlsx")
+write.xlsx(points3, "data/points3.xlsx")
+points3 <- read.xlsx("data/points3.xlsx", 1, encoding="UTF-8")
 
 # WordCloud 
-setwd("D:/workspace-Jwp/R/R-Project/01_Crowling")
-head(points$review)
-reply <- points$review
-write.csv(reply, "data/reply.csv", row.names = F)
-words <- readLines("data/reply.csv")
+head(points3$review)
+reply <- points3$review
+write.csv(reply, "data/reply3.csv", row.names = F)
+lines_r <- readLines("data/reply3.csv")
 
-words <- sapply(words, extractNoun, USE.NAMES = F)
+words <- sapply(lines_r, extractNoun, USE.NAMES = F)
+
 cdata <- unlist(words)
 words <- str_replace_all(cdata, "[^[:alpha:]]", "")
 words <- gsub(" ", "", words)
 words <- gsub("\\d+", "", words)
 words <- Filter(function(x) {nchar(x) >= 2}, words)
-head(words, 20)  
+words <- Filter(function(x) {nchar(x) <= 5}, words)
+head(words)
 
-write(unlist(words), "data/reply.txt")
-rev <- read.table("data/reply.txt")
-rev
-
+write(unlist(words), "data/reply3.txt")
+rev <- read.table("data/reply3.txt")
 wordcount <- table(rev)
-head(sort(wordcount, decreasing = T), 100)
+head(sort(wordcount, decreasing = T), 30)
 
 words <- gsub("영화", "", words)
 words <- gsub("진짜", "", words)
@@ -80,42 +85,38 @@ words <- gsub("ㅠㅠ", "", words)
 words <- gsub("이번", "", words)
 words <- gsub("뭔가", "", words)
 words <- gsub("이후", "", words)
-words <- gsub("스파이더맨", "", words)
+words <- gsub("관람객", "", words)
 
-write(unlist(words), "data/reply.txt")
-rev <- read.table("data/reply.txt")
+write(unlist(words), "data/reply3.txt")
+rev <- read.table("data/reply3.txt")
 
 wordcount <- table(rev)
-head(sort(wordcount, decreasing = T), 20)
+head(sort(wordcount, decreasing = T), 30)
 
-windows()
+#windows()
 windowsFonts(font=windowsFont("맑은 고딕"))
 
 palete <- brewer.pal(10, 'Paired')
 wordcloud(names(wordcount), freq=wordcount, scale = c(4,0.4), rot.per = 0.1,
-          min.freq = 3, random.order = F, random.color = T, colors = palete, family="font")
-#legend(0.1, 0.92, "NAVER 영화 [스파이더맨 : 파 프롬 홈]\n평점 댓글 WordCloud(19.07.02 ~ 19.07.04)", cex=1, fill=NA, border=NA,
-#       bg='navy', text.col='green', text.font=2, box.col='white')
-
+          min.freq = 1, random.order = F, random.color = T, colors = palete, family="font")
 
 # Star Rating Change over time
 # Change by Date
-head(points)
-star_date <- select(points, score, time)
-
+star_date <- select(points3, score, time)
 tmp <- strptime(star_date$time, "%Y.%m.%d %H")
 tmp <- paste0(tmp$mon+1, "월 ", tmp$mday, "일")
 star_date$time <- tmp
-star_date <- filter(star_date, str_sub(star_date$time, 1, 5) != "7월 5일")
+star_date <- filter(star_date, "7월 2일" == str_sub(star_date$time, 1, 5) |
+                               "7월 3일" == str_sub(star_date$time, 1, 5) |
+                               "7월 4일" == str_sub(star_date$time, 1, 5))
 star_date$score <- factor(star_date$score, levels=c(10:1))
-levels(star_date$score)
 
 star_date_bar <- star_date %>%
   group_by(time, score) %>%
   summarise(score_freq = n()) %>%
   arrange(time, score)
 star_date_bar
-  
+
 star_date_bar2 <- star_date_bar %>%  
   mutate(pct = score_freq/sum(score_freq)*100) %>%
   mutate(ylabel_b = str_c(score, "점 (", format(pct, digits=2), "%)")) %>%
@@ -124,18 +125,17 @@ star_date_bar2 <- star_date_bar %>%
   arrange(time, desc(score))
 star_date_bar2
 
-library(extrafont)
 windowsFonts(myfont = "맑은 고딕")
 theme_update(text=element_text(family="myfont"))
 
 # Bar Chart
 ggplot(data=star_date_bar2, aes(x=time, y=score_freq, fill=score)) + 
   geom_bar(stat="identity") +
-  geom_text(aes(y=ypos, label=ylabel_b), cex=4, color="black", family="myfont") +
+  geom_text(aes(y=ypos, label=ylabel_b), cex=3, color="black", family="myfont") +
   theme_light() +
-  xlab("날짜(일)") +
+  xlab("날짜(단위: 일)") +
   ylab("평점(1~10점)") +
-  ggtitle("[스파이더맨 : 파 프롬 홈]\n평점 빈도(19.07.02 ~ 19.07.04)") +
+  ggtitle("[ 알라딘 ]\n평점 빈도 (19.07.02 ~ 19.07.04)") +
   theme(plot.title=element_text(size=rel(1.5), face="bold", hjust=0.5, family="myfont")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -145,10 +145,10 @@ ggplot(data=star_date_bar2, aes(x=time, y=score_freq, fill=score)) +
 ggplot(data=star_date_bar2, aes(x=time, y=score_freq, fill=score)) + 
   geom_bar(stat="identity", position="dodge") +
   theme_light() +
-  geom_text(aes(y=rep(seq(360, 900, 60), 3), label=ylabel_b), cex=4, color="navy", family="myfont") +
-  xlab("날짜(일)") +
+  geom_text(aes(y=c(seq(50,120,10), rep(seq(50,130,10),2)), label=ylabel_b), cex=4, color="navy", family="myfont") +
+  xlab("날짜(단위: 일)") +
   ylab("평점(1~10점)") +
-  ggtitle("[스파이더맨 : 파 프롬 홈]\n평점 빈도(19.07.02 ~ 19.07.04)") +
+  ggtitle("[ 알라딘 ]\n평점 빈도 (19.07.02 ~ 19.07.04)") +
   theme(plot.title=element_text(size=rel(1.5), face="bold", hjust=0.5, family="myfont")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -167,9 +167,9 @@ star_date_line
 ggplot(star_date_line, aes(x=time, y=avg_score, color="red", group=1)) + 
   geom_line(color="red", size=1) +
   theme_light() +
-  xlab("날짜(일)") +
+  xlab("날짜(단위: 일)") +
   ylab("평점(1~10점)") +
-  ggtitle("[스파이더맨 : 파 프롬 홈]\n날짜에 따른 평점(평균) 변화(19.07.02 ~ 19.07.04)") +
+  ggtitle("[ 알라딘 ]\n날짜에 따른 평점(평균) 변화 (19.07.02 ~ 19.07.04)") +
   theme(plot.title=element_text(size=rel(1.5), face="bold", hjust=0.5, family="myfont")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -178,11 +178,13 @@ ggplot(star_date_line, aes(x=time, y=avg_score, color="red", group=1)) +
 
 # Change Hourly
 # Line Chart(1시간 단위)
-star_date2 <- select(points, score, time)
+star_date2 <- select(points3, score, time)
 tmp <- strptime(star_date2$time, "%Y.%m.%d %H:%M")
 tmp <- paste0(tmp$mon+1, "월 ", tmp$mday, "일 ", tmp$hour, "시")
 star_date2$time <- tmp
-star_date2 <- filter(star_date2, str_sub(star_date2$time, 1, 5) != "7월 5일")
+star_date2 <- filter(star_date2, "7월 2일" == str_sub(star_date2$time, 1, 5) |
+                                 "7월 3일" == str_sub(star_date2$time, 1, 5) |
+                                 "7월 4일" == str_sub(star_date2$time, 1, 5))
 star_date2$score <- as.character(star_date2$score)
 star_date2$score <- as.numeric(star_date2$score)
 
@@ -196,7 +198,7 @@ ggplot(star_hour_line1, aes(x=time, y=avg_score, group=1)) +
   theme_light() +
   xlab("시간(단위: 1시간)") +
   ylab("평점(1~10점)") +
-  ggtitle("[스파이더맨 : 파 프롬 홈]\n시간에 따른 평점(평균) 변화(19.07.02 ~ 19.07.04)") +
+  ggtitle("[ 알라딘 ]\n시간에 따른 평점(평균) 변화 (19.07.02 ~ 19.07.04)") +
   theme(plot.title=element_text(size=rel(1.5), face="bold", hjust=0.5, family="myfont")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -205,7 +207,7 @@ ggplot(star_hour_line1, aes(x=time, y=avg_score, group=1)) +
         axis.text.x = element_blank())
 
 # Line Chart(4시간 단위)
-star_date2 <- select(points, score, time)
+star_date2 <- select(points3, score, time)
 tmp <- strptime(star_date2$time, "%Y.%m.%d %H:%M")
 
 time_tmp <- rep(NA, length(tmp))
@@ -215,12 +217,13 @@ for(i in 1:length(tmp)) {
   ifelse(tmp$hour[i] <= 12, time_tmp[i] <- paste0(tmp[i]$mon+1, "월 ", tmp[i]$mday, "일 ", "8~12시"),
   ifelse(tmp$hour[i] <= 16, time_tmp[i] <- paste0(tmp[i]$mon+1, "월 ", tmp[i]$mday, "일 ", "12~16시"),
   ifelse(tmp$hour[i] <= 20, time_tmp[i] <- paste0(tmp[i]$mon+1, "월 ", tmp[i]$mday, "일 ", "16~20시"),
-  time_tmp[i] <- paste0(tmp$mon[i]+1, "월 ", tmp$mday[i], "일 ", "21~24시"))))))
+                            time_tmp[i] <- paste0(tmp$mon[i]+1, "월 ", tmp$mday[i], "일 ", "21~24시"))))))
 }
-time_tmp
 
 star_date2$time <- time_tmp
-star_date2 <- filter(star_date2, str_sub(star_date2$time, 1, 5) != "7월 5일")
+star_date2 <- filter(star_date2, "7월 2일" == str_sub(star_date2$time, 1, 5) |
+                                 "7월 3일" == str_sub(star_date2$time, 1, 5) |
+                                 "7월 4일" == str_sub(star_date2$time, 1, 5))
 star_date2$score <- as.character(star_date2$score)
 star_date2$score <- as.numeric(star_date2$score)
 
@@ -234,7 +237,7 @@ ggplot(star_hour_line2, aes(x=time, y=avg_score, group=1)) +
   theme_light() +
   xlab("시간(단위: 4시간)") +
   ylab("평점(1~10점)") +
-  ggtitle("[스파이더맨 : 파 프롬 홈]\n시간에 따른 평점(평균) 변화(19.07.02 ~ 19.07.04)") +
+  ggtitle("[토이스토리 4]\n시간에 따른 평점(평균) 변화 (19.07.02 ~ 19.07.04)") +
   theme(plot.title=element_text(size=rel(1.5), face="bold", hjust=0.5, family="myfont")) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
